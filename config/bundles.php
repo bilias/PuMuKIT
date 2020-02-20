2270fbc089b984214dcc8496ef2eea544521e9b8
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2020-02-20 07:28:15 +0100

    TTK-23328 update composer.json installing necessary dependencies for Twig

--- config/bundles.php
+++ config/bundles.php
@@ -29,4 +29,5 @@ return [
     Pumukit\WizardBundle\PumukitWizardBundle::class => ['all' => true],
     Pumukit\WorkflowBundle\PumukitWorkflowBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
